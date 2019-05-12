dec221ea31a513046201056e5f3665083e5ab255
grief8 <1349212501@qq.com>
2019-05-12 22:10:35 +0800

    some changes

--- app/src/main/java/com/se/crazymeme/MainActivity.java
+++ app/src/main/java/com/se/crazymeme/MainActivity.java
@@ -1,29 +1,408 @@
 package com.se.crazymeme;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button community;
+    private Button user_info;
+    private Button product;
+    private ImageView img1;
+    private ImageView img2;
+    private ImageView img3;
+//    private ImageView img4;
+//    private ImageView img5;
+    //轮播图片
+    private int[] imgArray;
+    private final int num_imgs = 20;
+    private Bitmap bitmap;
+//    private FaceHelper faceHelper;
+
+    //返回码，本地图库
+    private static final int RESULT_IMAGE = 100;
+    //返回码，相机
+    private static final int RESULT_CAMERA = 200;
+    //拍照后照片的Uri
+    private Uri imageUri;
+    private Uri cropUri;
+    public static final int CHOOSE_PHOTO = 2;
+    public static final int CROP_PHOTO = 3;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        community = (Button) findViewById(R.id.btn_shequ);
+        product = (Button) findViewById(R.id.btn_zhizuo) ;
+
+        RotateAnimation rotate  = new RotateAnimation(0f, 360f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        LinearInterpolator lin = new LinearInterpolator();
+        rotate.setInterpolator(lin);
+        rotate.setDuration(6000);//设置动画持续时间
+        rotate.setRepeatCount(-1);//设置重复次数
+        rotate.setFillAfter(true);//动画执行完后是否停留在执行完的状态
+        rotate.setStartOffset(10);//执行前的等待时间
+        img1 = (ImageView) findViewById(R.id.img1);
+        img1.setAnimation(rotate);
+        img2 = (ImageView) findViewById(R.id.img2);
+        img2.setAnimation(rotate);
+        img3 = (ImageView) findViewById(R.id.img3);
+        img3.setAnimation(rotate);
+//        img4 = (ImageView) findViewById(R.id.img4);
+//        img4.setAnimation(rotate);
+//        img5 = (ImageView) findViewById(R.id.img5);
+//        img5.setAnimation(rotate);
+
+        imgArray = new int[num_imgs];
+        int[] a = {R.drawable.a1, R.drawable.a2, R.drawable.a3, R.drawable.a4, R.drawable.a5,
+                R.drawable.a6, R.drawable.a7, R.drawable.a8, R.drawable.a9, R.drawable.a10, R.drawable.a11,
+                R.drawable.a12, R.drawable.a13, R.drawable.a14, R.drawable.a15, R.drawable.a16,
+                R.drawable.a17, R.drawable.a18,R.drawable.a19, R.drawable.a20};
+        imgArray = a;
+        img3.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public boolean onTouch(View v, MotionEvent event) {
+                int rand;
+                rand = getNum(0,num_imgs-1);
+                img1.setImageResource(imgArray[rand]);
+                img2.setImageResource(imgArray[(rand + 1)%num_imgs]);
+                img3.setImageResource(imgArray[(rand + 2)%num_imgs]);
+//                img4.setImageResource(imgArray[(rand + 3)%num_imgs]);
+//                img5.setImageResource(imgArray[(rand + 4)%num_imgs]);
+                return true;
+            }
+        });
+        community.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WebActivity.class);
+                startActivity(intent);
+            }
+        });
+        product.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Manufacture.class);
+                startActivity(intent);
+//                checkPermission();
+//                load();
+//                startPhotoCrop();
             }
         });
     }
 
+    //检查权限
+    public void checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            //发现没有权限，调用requestPermissions方法向用户申请权限，requestPermissions接收三个参数，第一个是context，第二个是一个String数组，我们把要申请的权限
+            //名放在数组中即可，第三个是请求码，只要是唯一值就行
+        } else {
+            openAlbum();//有权限就打开相册
+        }
+    }
+
+    public void openAlbum() {
+        //通过intent打开相册，使用startactivityForResult方法启动actvity，会返回到onActivityResult方法，所以我们还得复写onActivityResult方法
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent, CHOOSE_PHOTO);
+    }
+    //弹出窗口向用户申请权限
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);//弹出授权的窗口，这条语句也可以删除，没有影响
+        //获得了用户的授权结果，保存在grantResults中，判断grantResult中的结果来决定接下来的操作
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openAlbum();
+                } else {
+                    Toast.makeText(this, "授权失败，无法操作", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case CHOOSE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    if (Build.VERSION.SDK_INT >= 19) {
+                        handleImageOnkitKat(data);//高于4.4版本使用此方法处理图片
+                    } else {
+                        handleImageBeforeKitKat(data);//低于4.4版本使用此方法处理图片
+                    }
+                }
+                break;
+            case CROP_PHOTO:
+                try {
+                    Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                    img1.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @TargetApi(19)
+    private void handleImageOnkitKat(Intent data) {
+        String imagePath = null;
+        Uri uri = data.getData();
+        if (DocumentsContract.isDocumentUri(this, uri)) {
+            //如果是document类型的uri，则通过document id处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.documents".equals(uri.getAuthority())) {
+                String id = docId.split(":")[1];//解析出数字格式的id
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
+            } else if ("com.android,providers.downloads.documents".equals(uri.getAuthority())) {
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(docId));
+                imagePath = getImagePath(contentUri, null);
+            }
+
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            imagePath = getImagePath(uri, null);
+        }
+        displayImage(imagePath);
+    }
+
+    private void handleImageBeforeKitKat(Intent data) {
+        Uri uri = data.getData();
+        String imagePath = getImagePath(uri, null);
+        displayImage(imagePath);
+    }
+
+    //获得图片路径
+    public String getImagePath(Uri uri, String selection) {
+        String path = null;
+        Cursor cursor = getContentResolver().query(uri, null, selection, null, null);   //内容提供器
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));   //获取路径
+            }
+        }
+        cursor.close();
+        return path;
+    }
+
+    //展示图片
+    private void displayImage(String imagePath) {
+        if (imagePath != null) {
+            save(imagePath);
+            Bitmap bitImage = compressPixel(imagePath); //压缩图片
+//            Bitmap bitImage = BitmapFactory.decodeFile(imagePath);//格式化图片
+            bitImage = FaceHelper.genFaceBitmap(bitImage);
+            img1.setImageBitmap(bitImage);//为imageView设置图片
+//            startPhotoCrop();
+
+        } else {
+            Toast.makeText(MainActivity.this, "获取图片失败", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void save(String imagePath){
+        SharedPreferences.Editor editor = getSharedPreferences("data",MODE_PRIVATE).edit();//获得SHaredPreferences.Editor对象
+        editor.putBoolean("imageChange",true);//添加一个名为imageChange的boolean值，数值为true
+        editor.putString("imagePath",imagePath);//保存imagePath图片路径
+        editor.apply();//提交
+    }
+    private void load() {
+        SharedPreferences preferences = getSharedPreferences("data", MODE_PRIVATE);//获得SharedPreferences的对象
+        //括号里的判断是去找imageChange这个对应的数值，若是找不到，则是返回false，找到了的话就是我们上面定义的true，就会执行其中的语句
+        if (preferences.getBoolean("imageChange", false)) {
+            String imagePath = preferences.getString("imagePath", "");//取出保存的imagePath，若是找不到，则是返回一个空
+            imageUri = Uri.parse(imagePath);
+            displayImage(imagePath);//调用显示图片方法，为ImageView设置图片
+        }
+    }
+
+    /**
+     * 开启裁剪相片
+     */
+    public void startPhotoCrop() {
+        //创建file文件，用于存储剪裁后的照片
+//        File cropImage = new File(Environment.getExternalStorageDirectory(), "crop_image.jpg");
+        File cropImage = new File(getExternalCacheDir(), "crop_image.jpg");
+        try {
+            if (cropImage.exists()) {
+                cropImage.delete();
+            }
+            cropImage.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        cropUri = Uri.fromFile(cropImage);
+        cropUri = imageUri;
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        //设置源地址uri
+        intent.setDataAndType(cropUri, "image/*");
+        intent.putExtra("crop", "true");
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        intent.putExtra("outputX", 200);
+        intent.putExtra("outputY", 200);
+        intent.putExtra("scale", true);
+        //设置目的地址uri
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, cropUri);
+        //设置图片格式
+        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+        intent.putExtra("return-data", false);//data不需要返回,避免图片太大异常
+        intent.putExtra("noFaceDetection", true); // no face detection
+        startActivityForResult(intent, CROP_PHOTO);
+    }
+
+    private Bitmap compressPixel(String filePath){
+        Bitmap bmp = null;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        //setting inSampleSize value allows to load a scaled down version of the original image
+        options.inSampleSize = 2;
+
+        //inJustDecodeBounds set to false to load the actual bitmap
+        options.inJustDecodeBounds = false;
+        options.inTempStorage = new byte[16 * 1024];
+        try {
+            //load the bitmap from its path
+            bmp = BitmapFactory.decodeFile(filePath, options);
+            if (bmp == null) {
+
+                InputStream inputStream = null;
+                try {
+                    inputStream = new FileInputStream(filePath);
+                    BitmapFactory.decodeStream(inputStream, null, options);
+                    inputStream.close();
+                } catch (FileNotFoundException exception) {
+                    exception.printStackTrace();
+                } catch (IOException exception) {
+                    exception.printStackTrace();
+                }
+            }
+        } catch (OutOfMemoryError exception) {
+            exception.printStackTrace();
+        }finally {
+            return bmp;
+        }
+    }
+
+    private void delay(int ms) {
+        try {
+            Thread.currentThread();
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getNum(int startNum,int endNum){
+        if(endNum > startNum){
+            Random random = new Random();
+            return random.nextInt(endNum - startNum) + startNum;
+        }
+        return 0;
+    }
+//    private Uri imgPath() {
+//        Uri imageUri = null;
+//        String status = Environment.getExternalStorageState();
+//        if (status.equals(Environment.MEDIA_MOUNTED)) {
+//            //创建File对象，用于存储拍照后的照片
+//            File outputImage = new File(getExternalCacheDir(), "out_image.jpg");//SD卡的应用关联缓存目录
+//            try {
+//                if (outputImage.exists()) {
+//                    outputImage.delete();
+//                }
+//                outputImage.createNewFile();
+//                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//                    imageUri = FileProvider.getUriForFile(MainActivity.this,
+//                            "com.hanrui.android.fileprovider", outputImage);//添加这一句表示对目标应用临时授权该Uri所代表的文件
+//                } else {
+//                    imageUri = Uri.fromFile(outputImage);
+//                }
+//                //启动相机程序
+//                intent.putExtra(MediaStore.Images.Media.ORIENTATION, 0);
+//                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+//                startActivityForResult(intent, RESULT_CAMERA);
+//            } catch (Exception e) {
+//                // TODO Auto-generated catch block
+//                Toast.makeText(MainActivity.this, "没有找到储存目录", Toast.LENGTH_LONG).show();
+//            }
+//        } else {
+//            Toast.makeText(MainActivity.this, "没有储存卡", Toast.LENGTH_LONG).show();
+//        }
+//        dialog.dismiss();
+//        return imageUri;
+//    }
+//
+//    private void showDialogCustom() {
+//        //创建对话框
+//        String[] mCustomItems = new String[]{"本地相册", "相机拍照"};
+//        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+//        builder.setTitle("请选择：");
+//        builder.setItems(mCustomItems, new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                if (which == 0) {
+//                    //相册
+//
+//                } else if (which == 1) {
+//                    //照相机
+//
+//                }
+//            }
+//        });
+//        builder.create().show();
+//    }
+//
+//
+//    private void openAlbum() {
+//        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+//        intent.setType("image/*");
+//        startActivityForResult(intent, RESULT_IMAGE);//打开相册
+//
+//    }
 }
---
+++ app/src/main/java/com/se/crazymeme/WebActivity.java
@@ -0,0 +1,211 @@
+package com.se.crazymeme;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.JavascriptInterface;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class WebActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private ProgressBar progressBar;
+    private Context context;
+    private String url = "http://192.168.1.101:8000/";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Window window = getWindow();
+//隐藏标题栏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//隐藏状态栏
+//定义全屏参数
+        int flag = WindowManager.LayoutParams.FLAG_FULLSCREEN;
+//设置当前窗体为全屏显示
+        window.setFlags(flag, flag);
+
+        setContentView(R.layout.activity_web);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        context = this;
+        progressBar = findViewById(R.id.progressBar);
+        webView = findViewById(R.id.webview);
+
+        webView.loadUrl(url);
+
+        webView.addJavascriptInterface(this,"android");//添加js监听 这样html就能调用客户端
+        webView.setWebChromeClient(webChromeClient);
+        webView.setWebViewClient(webViewClient);
+        webView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                final WebView.HitTestResult hitTestResult = webView.getHitTestResult();
+                // 如果是图片类型或者是带有图片链接的类型
+                if(hitTestResult.getType()== WebView.HitTestResult.IMAGE_TYPE||
+                        hitTestResult.getType()== WebView.HitTestResult.SRC_IMAGE_ANCHOR_TYPE){
+                    // 弹出保存图片的对话框
+                    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                    builder.setTitle("提示");
+                    builder.setMessage("保存图片到本地");
+                    builder.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            String url = hitTestResult.getExtra();
+                            // 下载图片到本地
+                            DownPicUtil.downPic(url, new DownPicUtil.DownFinishListener(){
+
+                                @Override
+                                public void getDownPath(String s) {
+                                    Toast.makeText(context,"下载完成",Toast.LENGTH_LONG).show();
+                                    Message msg = Message.obtain();
+                                    msg.obj=s;
+                                    handler.sendMessage(msg);
+                                }
+                            });
+
+                        }
+                    });
+                    builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                        // 自动dismiss
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                        }
+                    });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+                return true;
+
+            }
+        });
+        WebSettings webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);//允许使用js
+        //设置加载进来的页面自适应手机屏幕
+//        webSettings.setUseWideViewPort(true);
+//        webSettings.setLoadWithOverviewMode(true);
+
+    }
+
+    private WebViewClient webViewClient = new WebViewClient(){
+        @Override
+        public void onPageFinished(WebView view, String url) {//页面加载完成
+            progressBar.setVisibility(View.INVISIBLE);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {//页面开始加载
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+//            Log.i("ansen","拦截url:"+url);
+//            if(url.equals("http://www.google.com/")){
+//                Toast.makeText(WebActivity.this,"国内不能访问google,拦截该url",Toast.LENGTH_LONG).show();
+//                return true;//表示我已经处理过了
+//            }
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+    };
+    //WebChromeClient主要辅助WebView处理Javascript的对话框、网站图标、网站title、加载进度等
+    private WebChromeClient webChromeClient = new WebChromeClient(){
+        //不支持js的alert弹窗，需要自己监听然后通过dialog弹窗
+        @Override
+        public boolean onJsAlert(WebView webView, String url, String message, JsResult result) {
+            AlertDialog.Builder localBuilder = new AlertDialog.Builder(webView.getContext());
+            localBuilder.setMessage(message).setPositiveButton("确定",null);
+            localBuilder.setCancelable(false);
+            localBuilder.create().show();
+
+            //注意:
+            //必须要这一句代码:result.confirm()表示:
+            //处理结果为确定状态同时唤醒WebCore线程
+            //否则不能继续点击按钮
+            result.confirm();
+            return true;
+        }
+
+        //获取网页标题
+        @Override
+        public void onReceivedTitle(WebView view, String title) {
+            super.onReceivedTitle(view, title);
+            Log.i("ansen","网页标题:"+title);
+        }
+
+        //加载进度回调
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            progressBar.setProgress(newProgress);
+        }
+    };
+
+
+    @SuppressLint("HandlerLeak")
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            String picFile = (String) msg.obj;
+//            String[] split = picFile.split("/");
+//            String fileName = split[split.length-1];
+//            try {
+//                MediaStore.Images.Media.insertImage(getApplicationContext().getContentResolver(), picFile, fileName, null);
+//            } catch (FileNotFoundException e) {
+//                e.printStackTrace();
+//            }
+            // 最后通知图库更新
+            getApplicationContext().sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse("file://" + picFile)));
+            Toast.makeText(context,"图片保存图库成功",Toast.LENGTH_LONG).show();
+        }
+    };
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        Log.i("ansen","是否有上一个页面:"+webView.canGoBack());
+        if (webView.canGoBack() && keyCode == KeyEvent.KEYCODE_BACK){//点击返回按钮的时候判断有没有上一页
+            webView.goBack(); // goBack()表示返回webView的上一页面
+            return true;
+        }
+        return super.onKeyDown(keyCode,event);
+    }
+
+    /**
+     * JS调用android的方法
+     * @param str
+     * @return
+     */
+    @JavascriptInterface //仍然必不可少
+    public void  getClient(String str){
+        Log.i("ansen","html调用客户端:"+str);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        //释放资源
+        webView.destroy();
+        webView=null;
+    }
+}
